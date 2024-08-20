fc1124e244e23f0799bc2ba9ad6846dc181f7d64
levywanke <levywanke@gmai.com>
2024-08-20 21:51:21 +0300

    node jas basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author levy wanyonyi <https://github.com/levywanke>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
